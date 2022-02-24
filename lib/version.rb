8a04f2f505acc16fd8e86320106b573a4ade484a
Maarten van der Velden <m.van.der.velden@catawiki.nl>
2022-02-24 15:31:35 +0100

    Fixed SwiftLint hash for new SwiftLint version
    This fixes issues when running the plugin, as mentioned in #178

--- lib/version.rb
+++ lib/version.rb
@@ -3,5 +3,5 @@
 module DangerSwiftlint
   VERSION = '0.30.0'
   SWIFTLINT_VERSION = '0.46.2'
-  SWIFTLINT_HASH = '4eaeabbb43b308975d16e3d9869880dc'
+  SWIFTLINT_HASH = '20dbcc13788993dd27b1076a13ca647d'
 end
