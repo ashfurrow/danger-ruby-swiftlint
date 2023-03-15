30dfbe3a9279e1f4a271050ff49602fd4ae02720
Ash Furrow <ash@ashfurrow.com>
2023-03-15 19:29:06 -0300

    Version bump.

--- lib/version.rb
+++ lib/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module DangerSwiftlint
-  VERSION = '0.30.2'
+  VERSION = '0.32.0'
   SWIFTLINT_VERSION = '0.50.3'
   SWIFTLINT_HASH = 'bd6459b120cc59cb9d939f157f89932e'
 end
